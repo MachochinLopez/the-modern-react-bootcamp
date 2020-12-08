29d415c72ab58ebc495eb0fe09e1e589d21cfaab
Oscar López <machochinnio@hotmail.com>
2020-12-08 12:29:21 -0600

    gitignore and Introducing imports and exportgitignore and Introducing imports and exportss

---
+++ 4 - Introducing Create React App/imports-and-exports-crash-course/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful () {
+    console.log('BEING HELPFUL YEAHH!');
+}
+
+function sort () {
+    console.log('ALL SORTED');
+}
+
+function sing () {
+    console.log('LALALA');
+}
+
+export default helpful;
+export { sort, sing };
